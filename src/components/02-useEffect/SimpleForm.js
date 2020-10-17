65b3affaac1bfa58a06d13eb3f8dd6c2b1b205a9
danielislas3 <danielislas3@live.com>
2020-10-17 12:38:57 -0500

    useEffect

--- src/components/02-useEffect/SimpleForm.js
+++ src/components/02-useEffect/SimpleForm.js
@@ -2,30 +2,48 @@ import React, { useEffect, useState } from 'react';
 import '../index.css';
 
 export const SimpleForm = () => {
-  
-  
-  
   const [formState, setFormState] = useState({
     name: '',
     email: '',
   });
-  
-  
+
   const { name, email } = formState;
-  
+
+  useEffect(() => {}, [formState]);
+
   useEffect(() => {
-    console.log('holi');
-  }, []);
-  
-  const handleInputChange=(e)=>{
-    console.log(e.target.name);
-  }
+    console.log('emal ejecutado ');
+  }, [email]);
+
+  const handleInputChange = ({ target }) => {
+    setFormState({
+      ...formState,
+      [target.name]: target.value,
+    });
+  };
   return (
     <div>
       <h1>UseEffect</h1>
-      <hr/>
+      <hr />
       <div>
-        <input type='text' name='name' className='form-control' placeholder="tu nombre" value={name} onChange={handleInputChange}/>
+        <input
+          type='text'
+          name='name'
+          className='form-control'
+          placeholder='tu nombre'
+          value={name}
+          onChange={handleInputChange}
+        />
+      </div>
+      <div>
+        <input
+          type='email'
+          name='email'
+          className='form-control'
+          placeholder='tu email'
+          value={email}
+          onChange={handleInputChange}
+        />
       </div>
     </div>
   );
