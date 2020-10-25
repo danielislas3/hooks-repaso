0cccd2896ab974b7faa970eb872be055a64467fb
danielislas3 <danielislas3@live.com>
2020-10-25 11:49:54 -0600

    useLayout

--- src/components/05-useLayoutEffect/Layout.js
+++ src/components/05-useLayoutEffect/Layout.js
@@ -3,15 +3,23 @@ import '../index.css';
 import './layout.css';
 import { useFetch } from '../../hooks/useFetch';
 import { useCountrer } from '../../hooks/useCounter';
+import { useLayoutEffect } from 'react';
+import { useRef } from 'react';
 
 export const Layout = () => {
   const { counter, increment, decrement } = useCountrer(1);
   const { loading, data } = useFetch(
     `https://www.breakingbadapi.com/api/quotes/${counter}`
   );
-// se evalua si data es null,si da true entonces extrae author, quote de data
+  // se evalua si data es null,si da true entonces extrae author, quote de data
   const { author, quote } = !!data && data[0];
-  
+
+  const pTag = useRef();
+  useLayoutEffect(() => {
+    console.log('====================================');
+    console.log(pTag.current.getBoundingClientRect());
+    console.log('====================================');
+  }, [quote]);
 
   return (
     <>
@@ -19,7 +27,9 @@ export const Layout = () => {
       <hr />
 
       <blockquote className='blockquote text-right'>
-        <p className='mb-8'>{quote}</p>
+        <p ref={pTag} className='mb-8'>
+          {quote}
+        </p>
         <footer className='blockquote-footer'> {author} </footer>
       </blockquote>
 
